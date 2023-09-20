7eb18ed1dcda55adec01cc960ac3d547fc849b39
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-09-20 09:16:02 +0300

    part 2

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,9 +5,39 @@ function Portfolio() {
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
-      <li className="portfolio__project"><a target="_blank" rel="noopener noreferrer" className="portfolio__link" href="https://github.com/ksenia-gal/how-to-learn">Статичный сайт</a></li>
-      <li className="portfolio__project"><a target="_blank" rel="noopener noreferrer" className="portfolio__link" href="https://github.com/ksenia-gal/russian-traveln">Адаптивный сайт</a></li>
-      <li className="portfolio__project"><a target="_blank" rel="noopener noreferrer" className="portfolio__link" href="https://github.com/ksenia-gal/react-mesto-api-full-gha">Одностраничное приложение</a></li>
+        <li className="portfolio__project">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="portfolio__link"
+            href="https://github.com/ksenia-gal/how-to-learn"
+          >
+            <p className="portfolio__text">Статичный сайт</p>
+            <div className="portfolio__icon"></div>
+          </a>
+        </li>
+        <li className="portfolio__project">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="portfolio__link"
+            href="https://github.com/ksenia-gal/russian-traveln"
+          >
+            <p className="portfolio__text">Адаптивный сайт</p>
+            <div className="portfolio__icon"></div>
+          </a>
+        </li>
+        <li className="portfolio__project">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="portfolio__link"
+            href="https://github.com/ksenia-gal/react-mesto-api-full-gha"
+          >
+            <p className="portfolio__text">Одностраничное приложение</p>
+            <div className="portfolio__icon"></div>
+          </a>
+        </li>
       </ul>
     </section>
   );
